412d530b29b51bce8d77d9c44211913abfaec07f
Douglas Tott <douglasifbaianosb@gmail.com>
2019-01-06 12:13:59 -0300

    Repositório Udemy

--- Curso-JavaScript-COD3R/Aulas/fundamentos-js/try_catch_throw.js
+++ Curso-JavaScript-COD3R/Aulas/fundamentos-js/try_catch_throw.js
@@ -9,25 +9,30 @@ function tratarErro (erro) {
 		msg: erro.mensagem,
 		data: new Data //Criando uma nova instância de data
 	}*/
-	throw 'Ocorreu um Erro'; //Lança um Erro
+	throw 'Ocorreu um Erro'; 
+	//Lança um Erro
 }
 
 
-function nomeGritar () {
+function nomeGritar(obj) {
+
 	try {
 
 		//Mostrando um objeto nome
-		console.log(obj.name.uppercase() + '!!!');
+		console.log(obj.name.toUpperCase() + '!!!');
 
-	} catch ( e ) {// O e é para receber um valor de parâmetro, que seria o retorno da função de Tratar erro
+	} catch (e) {// O e é para receber um valor de parâmetro, que seria o retorno da função de Tratar erro
 
 		//Chamando a Função de Erro
-		tratarErro( e );
+		tratarErro(e);
+
+	} finally {
+		console.log('Final');//Mostra a mensagem de qualquer jeito, mesmo não ocorrendo erro
 	}
 }
 
 // Colocando o nome diferente do da função nomeGritar que é name, para ocorrer o erro
-const obj  = { nome:'Roberto' };
+const obj  = { nome: 'Roberto' }
 
 //Chamando função
-nomeGritar();
+nomeGritar(obj);
