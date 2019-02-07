bab7cfd6636c1c6f183df54e0b3b7fbc097d2035
Douglas Tott <douglasifbaianosb@gmail.com>
2019-02-07 10:11:18 -0300

    Repositório Udemy

---
+++ Curso-JavaScript-COD3R/Aulas/array/criandoForEach.js
@@ -0,0 +1,18 @@
+//Criando o forEach próprio
+Object.prototype.forEach2 = function(callback) {
+
+    for( let i = 0; i < this.length; i++ ) {
+        //Chamando função callback passando 3 parâmentros: o primeiro parâmetro é o próprio valor(const pessoas), o segundo é o indice(posição) e o ultimo é array completo
+        callback(this[i], i, this);
+    }
+}
+
+const pessoas = ['Andrea', 'Marlos', 'Carlos', 'MeninaLinda'];
+
+pessoas.forEach2(function(nome, indice) {
+    
+    console.log(`${indice + 1}) ${nome}`)
+})
+
+//Com Arrow Function
+//pessoas.forEach2(elemento => console.log(elemento)) 
